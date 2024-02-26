b9131726ad1fa7daac6ca3ec924932a790f0b011
Omar A <114465136+omaroaalj@users.noreply.github.com>
2024-02-26 12:23:44 -0700

    Update Contact link

--- financeWebApp/financeWebsite/urls.py
+++ financeWebApp/financeWebsite/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "financeWebsite"
 urlpatterns = [
     path("", views.index, name="index"),
     path("contact/", views.contact, name="contact"),
