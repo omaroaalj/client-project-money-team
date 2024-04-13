221647c2b2bac53b69e9a96012cb095d1352c696
Omar A <114465136+omaroaalj@users.noreply.github.com>
2024-04-12 22:00:37 -0600

    Update contact page design

--- financeWebApp/financeWebsite/forms.py
+++ financeWebApp/financeWebsite/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label="Enter your name:", max_length=100, required=True)
-    email = forms.EmailField(label="Enter your email:", required=True)
-    message = forms.CharField(label="Enter your message:", max_length=200, required=True)
+    name = forms.CharField(label="Your name:", max_length=100, required=True)
+    email = forms.EmailField(label="Your email:", required=True)
+    message = forms.CharField(label="Your message:", widget=forms.Textarea, max_length=200, required=True)
