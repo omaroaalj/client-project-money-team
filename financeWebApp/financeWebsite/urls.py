568a201f27d0cb7d2f1ae42244f1e77f6f80b536
Omar A <114465136+omaroaalj@users.noreply.github.com>
2024-04-17 12:16:09 -0600

    Remove portfolio URL

--- financeWebApp/financeWebsite/urls.py
+++ financeWebApp/financeWebsite/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path("alternativeInvestments/", views.alternativeInvestments, name="alternativeInvestments"),
     path("lifeInsurance/", views.lifeInsurance, name="lifeInsurance"),
     path("healthcare/", views.healthcare, name="healthcare"),
-    path("portfolio/", views.portfolio, name="portfolio"),
     path("submitContact/", views.submit_contact, name="submit_contact"),
     path("thanks/", views.thanks, name="thanks"),
 ]
