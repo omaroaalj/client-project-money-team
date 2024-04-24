ecb4619374262c6335c0d3da1fa2f65f9ee07a8e
Omar A <114465136+omaroaalj@users.noreply.github.com>
2024-04-24 13:38:35 -0600

    Update admin layout, add contact date and add more tests

--- financeWebApp/financeWebsite/admin.py
+++ financeWebApp/financeWebsite/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from .models import ContactEntry
+
 
 # Register your models here.
-
+class ContactEntryAdmin(admin.ModelAdmin):
+    list_display = ["name", "date"]
 
 # Current Client database / newly added clients
 
+
+admin.site.register(ContactEntry, ContactEntryAdmin)
--- financeWebApp/financeWebsite/models.py
+++ financeWebApp/financeWebsite/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from django.db.models.functions import Now
 
 # Create your models here.
+
 
 class ContactEntry(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     message = models.CharField(max_length=200)
+    date = models.DateTimeField(db_default=Now())
+
+    def __str__(self):
+        return self.name + " (" + str(self.date) + ")"
+
+    class Meta:
+        verbose_name_plural = "Contact Entries"
