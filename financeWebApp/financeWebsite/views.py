c80110b1ee2c3846548b26d5bf63d22177c92cd1
drizzy2004 <157053650+drizzy2004@users.noreply.github.com>
2024-04-29 16:24:09 -0600

    update views.py

--- financeWebApp/financeWebsite/views.py
+++ financeWebApp/financeWebsite/views.py
@@ -6,27 +6,36 @@ from .models import ContactEntry
 from .forms import ContactForm
 
 # Create your views here.
+
+
 def index(request):
     return render(request, "financeWebsite/landingPage.html")
+
 
 def contact(request):
     form = ContactForm()
     return render(request, "financeWebsite/contact.html", {"form": form})
 
+
 def about(request):
     return render(request, "financeWebsite/about.html")
+
 
 def services(request):
     return render(request, "financeWebsite/services.html")
 
+
 def retirement(request):
     return render(request,"financeWebsite/retirement.html")
+
 
 def alternativeInvestments(request):
     return render(request,"financeWebsite/alternativeInvestments.html")
 
+
 def lifeInsurance(request):
     return render(request,"financeWebsite/lifeInsurance.html")
+
 
 def healthcare(request):
     return render(request,"financeWebsite/healthcare.html")
